4cf4e697bf77108c860d929d39cbbe0f062b838d
AlbertoOlmos <albrtho94@gmail.com>
2023-05-10 20:37:33 -0600

    thumbnail de la foto en el menu de navegacion

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light pb-4">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -13,9 +13,14 @@
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
-            {{ auth()->user()->encargado->nombre }}
+          {{-- si existe una foto en la base de datos --}}
+          @if (auth()->user()->image != null)
+          <img src="{{ asset('/storage/images/perfiles/'.auth()->user()->image->path) }}" alt="foto de perfil" width="50" height="50" class="rounded-circle">
+          @else
+          <img src="{{ asset('blank-profile-picture-gb21b7bce2_640.png') }}" alt="foto de perfil" width="50" height="50" class="rounded-circle">
+          @endif
+          {{ auth()->user()->encargado->nombre }} 
           <i class="fas fa-caret-down"></i>
-          
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <div class="dropdown-divider"></div>
@@ -23,17 +28,13 @@
             {{ auth()->user()->roles[0]->tipo }}
           </span>
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">
+          <a href="#" class="dropdown-item dropdown-footer pb-1">
             <form action="{{ route('perfil.show') }}" method="get">
                 <button style="border: none; background:none;"><i class="fas fa-user"></i> Perfil</button>
             </form>
           </a>
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-           
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">
+          <a href="#" class="dropdown-item dropdown-footer pb-1">
             <form action="{{ url('/logout') }}" method="post">
                 @csrf
                 <button style="border: none; background:none;"><i class="fas fa-sign-out-alt"></i> Cerrar sesión</button>
